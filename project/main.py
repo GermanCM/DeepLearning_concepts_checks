a93af3299986db54c64c47991582bd4b0bc12f91
Cabrera Martínez <gcabreram@minsait.com>
2020-01-24 11:34:44 +0100

    mlp 4 time series + gpu enabled TF training

--- project/main.py
+++ project/main.py
@@ -1,10 +1,12 @@
 # Main script where to test simple Tensorflow and Keras examples
 #%%
+'''
 import tensorflow as tf
 
 node1 = tf.constant(3.0, dtype=tf.float32)
 node2 = tf.constant(4.0)
 node3 = tf.add(node1, node2)
-sess = tf.Session()
+sess = tf.Session() --> https://github.com/tensorflow/community/blob/master/rfcs/20180918-functions-not-sessions-20.md 
 print("node3:", node3)
 print("sess.run(node3):", sess.run(node3))
+'''
